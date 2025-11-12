1345d0bbd0de7e2c63d00d31561bf716778ad617
Guilherme Alencar <163006803+GuilhermeAlencar22@users.noreply.github.com>
2025-11-12 16:21:18 -0300

    Update script.sh

--- desafio1/client/script.sh
+++ desafio1/client/script.sh
@@ -1,11 +1,6 @@
-#!/bin/bash
-
-# Espera o servidor subir
 sleep 5
-
-# Faz requisições a cada 5 segundos
 while true; do
-  echo "Cliente consultando o servidor..."
+  echo "Cliente olhando o servidor..."
   curl -s web:8080
   echo -e "\n---"
   sleep 5
