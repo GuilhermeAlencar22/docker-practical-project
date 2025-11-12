6105a5302447d913baf512f303df449023099df9
Guilherme Alencar <163006803+GuilhermeAlencar22@users.noreply.github.com>
2025-11-12 16:25:08 -0300

    Update run-containers.sh

--- desafio1/run-containers.sh
+++ desafio1/run-containers.sh
@@ -1,9 +1,6 @@
-#!/bin/bash
-
 docker build -t flask-server ./web
 docker build -t curl-client ./client
 
 docker network inspect rede-flask >/dev/null 2>&1 || docker network create rede-flask
-
 docker run -d --name web --network rede-flask -p 8080:8080 flask-server
 docker run -d --name client --network rede-flask curl-client
